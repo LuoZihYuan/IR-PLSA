f2b636630b6d6b1575efb23abbd62d29c6458d6d
LuoZihYuan <fudan28024@gmail.com>
2018-11-08 00:06:09 +0800

    :sparkles: feat(plsa.py): finish P(Tk|dj)

--- plsa.py
+++ plsa.py
@@ -6,7 +6,7 @@ import gc
 from collections import Counter
 import numpy as np
 
-TOPIC_SIZE = 256
+TOPIC_SIZE = 10
 LEXICON_SIZE = 51253
 COLLECTION_SIZE = 18461
 
@@ -14,28 +14,49 @@ def m_step(collection: list, p_wt: np.ndarray, p_td: np.ndarray):
   old_wt = p_wt.copy(); old_td = p_td.copy()
 
   p_wt_devisor = np.zeros(old_wt.shape[1], dtype=float) # (TOPIC_SIZE,)
-  p_wt_dividend = np.zeros(old_wt.shape, dtype=float) # (51253, TOPIC_SIZE)
-  for i_prime in tqdm(range(old_wt.shape[0])):
-  # for i_prime in range(old_wt.shape[0]):
+  p_wt_devidend = np.zeros(old_wt.shape, dtype=float) # (51253, TOPIC_SIZE)
+  # for i_prime in tqdm(range(old_wt.shape[0])):
+  for i_prime in range(old_wt.shape[0]):
 
-    # C(Wi', dj) || dj(D
+    # C(Wi', dj) -> dj(D
     word = str(i_prime)
     c_wd = np.zeros((1, len(collection)), dtype=int)
     for j in range(len(collection)):
       c_wd[0][j] = collection[j][word]
 
-    # P(Tk|Wi', dj) || dj(D
+    # P(Tk|Wi', dj) -> dj(D
     p_twd_devisor = np.dot(old_wt[i_prime], old_td.T)
     p_twd_devidend = old_wt[i_prime] * old_td
     p_twd = (p_twd_devidend.T / p_twd_devisor).T
+
+    # C(Wi', dj) * P(Tk|Wi', dj) -> dj(D
     sum_cp_d = np.dot(c_wd, p_twd)[0]
-    p_wt_dividend[i_prime] = sum_cp_d
+
+    p_wt_devidend[i_prime] = sum_cp_d
     p_wt_devisor += sum_cp_d
+  p_wt = p_wt_devidend / p_wt_devisor
 
-  p_wt = p_wt_dividend / p_wt_devisor
+  p_td_devisor = np.zeros(len(collection), dtype=float) # (18461,)
+  p_td_devidend = np.zeros(old_td.shape, dtype=float) # (18461, TOPIC_SIZE)
+  # for j in tqdm(range(old_td.shape[0])):
+  for j in range(old_td.shape[0]):
 
+    # C(Wi', dj) -> i=1~|V|
+    c_wd = np.zeros((1, old_wt.shape[0]), dtype=int)
+    for key, value in collection[j].items():
+      c_wd[0][int(key)] = value
+      p_td_devisor[j] += value
 
+    # P(Tk|Wi', dj) -> i=1~|V|
+    p_twd_devisor = np.dot(old_wt, old_td[j])
+    p_twd_devidend = old_wt * old_td[j]
+    p_twd = (p_twd_devidend.T / p_twd_devisor).T
 
+    # C(Wi', dj) * P(Tk|Wi', dj) -> i=1~|V|
+    sum_cp_v = np.dot(c_wd, p_twd)[0]
+
+    p_td_devidend[j] = sum_cp_v
+  p_td = (p_td_devidend.T / p_td_devisor).T
 
 def main():
   collection = []
