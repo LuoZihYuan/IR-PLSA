987e8f6241383848fe65493e8928a50ecca0478d
LuoZihYuan <fudan28024@gmail.com>
2018-11-13 13:03:17 +0800

    :bug: fix(em_gpu.py): cupy doesn't support errstate

--- em_gpu.py
+++ em_gpu.py
@@ -12,62 +12,59 @@ def em(collection: list, p_wt: cp.ndarray, p_td: cp.ndarray) -> (cp.ndarray, cp.
   # P(Wi|Tk)
   p_wt_devisor = cp.zeros(p_wt.shape[1], dtype=float) # (TOPIC_SIZE,)
   p_wt_devidend = cp.zeros(p_wt.shape, dtype=float) # (51253, TOPIC_SIZE)
-  with cp.errstate(divide='ignore'):
-    for i_prime in tqdm(range(p_wt.shape[0]), desc='EM_P(Wi|Tk)'):
+  for i_prime in tqdm(range(p_wt.shape[0]), desc='EM_P(Wi|Tk)'):
 
-      # C(Wi', dj) -> dj(D
-      word = str(i_prime)
-      c_wd = [[collection[j][word] for j in range(len(collection))]]
+    # C(Wi', dj) -> dj(D
+    word = str(i_prime)
+    c_wd = [[collection[j][word] for j in range(len(collection))]]
 
-      # P(Tk|Wi', dj) -> dj(D
-      p_twd_devisor = cp.dot(p_wt[i_prime], p_td.T)
-      p_twd_devidend = p_wt[i_prime] * p_td
-      p_twd = cp.nan_to_num(p_twd_devidend.T / p_twd_devisor).T
+    # P(Tk|Wi', dj) -> dj(D
+    p_twd_devisor = cp.dot(p_wt[i_prime], p_td.T)
+    p_twd_devidend = p_wt[i_prime] * p_td
+    p_twd = cp.nan_to_num(p_twd_devidend.T / p_twd_devisor).T
 
-      # C(Wi', dj) * P(Tk|Wi', dj) -> dj(D
-      sum_cp_d = cp.dot(c_wd, p_twd)[0]
+    # C(Wi', dj) * P(Tk|Wi', dj) -> dj(D
+    sum_cp_d = cp.dot(c_wd, p_twd)[0]
 
-      p_wt_devidend[i_prime] = sum_cp_d
-      p_wt_devisor += sum_cp_d
-    new_wt = p_wt_devidend / p_wt_devisor
+    p_wt_devidend[i_prime] = sum_cp_d
+    p_wt_devisor += sum_cp_d
+  new_wt = p_wt_devidend / p_wt_devisor
 
   # P(Tk|dj)
   p_td_devisor = cp.zeros(len(collection), dtype=float) # (18461,)
   p_td_devidend = cp.zeros(p_td.shape, dtype=float) # (18461, TOPIC_SIZE)
-  with cp.errstate(divide='ignore'):
-    for j in tqdm(range(p_td.shape[0]), desc='EM_P(Tk|dj)'):
+  for j in tqdm(range(p_td.shape[0]), desc='EM_P(Tk|dj)'):
 
-      # C(Wi', dj) -> i=1~|V|
-      c_wd = cp.zeros((1, p_wt.shape[0]), dtype=int)
-      for key, value in collection[j].items():
-        c_wd[0][int(key)] = value
-        p_td_devisor[j] += value
+    # C(Wi', dj) -> i=1~|V|
+    c_wd = cp.zeros((1, p_wt.shape[0]), dtype=int)
+    for key, value in collection[j].items():
+      c_wd[0][int(key)] = value
+      p_td_devisor[j] += value
 
-      # P(Tk|Wi', dj) -> i=1~|V|
-      p_twd_devisor = cp.dot(p_wt, p_td[j])
-      p_twd_devidend = p_wt * p_td[j]
-      p_twd = cp.nan_to_num(p_twd_devidend.T / p_twd_devisor).T
+    # P(Tk|Wi', dj) -> i=1~|V|
+    p_twd_devisor = cp.dot(p_wt, p_td[j])
+    p_twd_devidend = p_wt * p_td[j]
+    p_twd = cp.nan_to_num(p_twd_devidend.T / p_twd_devisor).T
 
-      # C(Wi', dj) * P(Tk|Wi', dj) -> i=1~|V|
-      sum_cp_v = cp.dot(c_wd, p_twd)[0]
+    # C(Wi', dj) * P(Tk|Wi', dj) -> i=1~|V|
+    sum_cp_v = cp.dot(c_wd, p_twd)[0]
 
-      p_td_devidend[j] = sum_cp_v
-    new_td = (p_td_devidend.T / p_td_devisor).T
+    p_td_devidend[j] = sum_cp_v
+  new_td = (p_td_devidend.T / p_td_devisor).T
 
     return new_wt, new_td
 
 def log_likelihood(collection: list, p_wt: cp.ndarray, p_td: cp.ndarray) -> float:
 
   likelihood = 0.
-  with cp.errstate(divide='ignore'):
-    for i in tqdm(range(p_wt.shape[0]), desc='Log_like'):
-      # C(Wi', dj) -> dj(D
-      word = str(i)
-      c_wd = [collection[j][word] for j in range(len(collection))]
+  for i in tqdm(range(p_wt.shape[0]), desc='Log_like'):
+    # C(Wi', dj) -> dj(D
+    word = str(i)
+    c_wd = [collection[j][word] for j in range(len(collection))]
 
-      # P(Wi|Tk)P(Tk|dj) -> k=1~K
-      sum_pp_k = cp.dot(p_wt[i], p_td.T)
-      likelihood += cp.dot(c_wd, cp.nan_to_num(cp.log(sum_pp_k)))
+    # P(Wi|Tk)P(Tk|dj) -> k=1~K
+    sum_pp_k = cp.dot(p_wt[i], p_td.T)
+    likelihood += cp.dot(c_wd, cp.nan_to_num(cp.log(sum_pp_k)))
 
   return likelihood
 
